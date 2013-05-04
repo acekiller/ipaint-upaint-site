4e6a84e3e7285d04539d0fd121f0aa7af3eeb456
Jesse Squires <jesse.d.squires@gmail.com>
2013-05-03 23:57:00 -0400

    style tweaks

--- support/index.php
+++ support/index.php
@@ -9,11 +9,12 @@
 		<div class="container" id="main">
 			<div class="row center" id="support">
 				<div class="span7 offset2">
-					<h1 class="text-left">Support <small> <a href="/">iPaint uPaint</a></small></h1>
-					<h3 class="text-left">Email <small> <a href="mailto:support@hexedbits.com">support [at] hexedbits [dot] com</a></small></h3>
-					<h3 class="text-left">Facebook <small> <a href="http://facebook.com/hexedbits" target="_blank">hexedbits</a></small></h3>
-					<h3 class="text-left">Twitter <small> <a href="http://twitter.com/hexedbits" target="_blank">@hexedbits</a></small></h3>
-					<h3 class="text-left">Web <small> <a href="http://hexedbits.com" target="_blank">hexedbits.com</a></small></h3>
+					<h3 class="text-left">Support <small> <a href="/">iPaint uPaint</a></small></h3>
+					<br />
+					<h4 class="text-left">Email <small> <a href="mailto:support@hexedbits.com">support [at] hexedbits [dot] com</a></small></h4>
+					<h4 class="text-left">Facebook <small> <a href="http://facebook.com/hexedbits" target="_blank">hexedbits</a></small></h4>
+					<h4 class="text-left">Twitter <small> <a href="http://twitter.com/hexedbits" target="_blank">@hexedbits</a></small></h4>
+					<h4 class="text-left">Web <small> <a href="http://hexedbits.com" target="_blank">hexedbits.com</a></small></h4>
 					<hr>
 				</div> <!-- span -->
 			</div> <!-- row -->
