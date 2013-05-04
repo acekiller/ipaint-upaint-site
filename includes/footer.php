10081e906cc396cf1a0d121114222965e1af1bed
Jesse Squires <jesse.d.squires@gmail.com>
2013-05-04 00:34:19 -0400

    more style tweaks. new privacy page.

--- includes/footer.php
+++ includes/footer.php
@@ -8,7 +8,10 @@
 				 		} 
 				 	?>">
 					<small>&copy; 2013 <a href="http://bit.ly/0x29A" target="_blank">Hexed Bits</a>
-						<span class="<?php if ($thisPage != "home") echo "hide"; ?>"> | <a href="/support"> Support</a></span>
+						<span class="<?php if ($thisPage != "home") echo "hide"; ?>"> 
+							&nbsp;&bull;&nbsp;&nbsp;<a href="/support">Support</a>
+							&nbsp;&bull;&nbsp;&nbsp;<a href="/privacy">Privacy</a>
+						</span>
 					</small>
 					<a href="http://bit.ly/0x29A" target="_blank">
 						<img id="hb" class="center" src="/img/hb@2x.png" title="Hexed Bits" alt="Hexed Bits"/>
---
+++ privacy/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php $thisPage="privacy"; ?>
+<html lang="en">
+	<head>
+		<?php include('../includes/head.php'); ?>
+		<title>iPaint uPaint | Privacy</title>
+	</head>
+	<body>
+		<div class="container" id="main">
+			<div class="row center" id="support">
+				<div class="span7 offset2">
+					<h3 class="text-left">Privacy <small> <a href="/">iPaint uPaint</a></small></h3>
+					<br />
+					<p class="text-left">iPaint uPaint does not track or collect any personal data. Why? Because we don't care.</p>
+					<p class="text-left">iPaint uPaint only transmits anonymous usage information via the <a href="http://testflightapp.com" target="_blank">TestFlight Live</a> SDK. This information helps improve the app and fix bugs. All data gathered is subject to the <a href="https://testflightapp.com/privacy/" target="_blank">TestFlight Privacy</a> Policy.</p>
+					<hr>
+				</div> <!-- span -->
+			</div> <!-- row -->
+			<?php include('../includes/footer.php'); ?>
+		</div> <!-- container -->
+		<?php include('../includes/foot.php'); ?>
+	</body>
+</html>
